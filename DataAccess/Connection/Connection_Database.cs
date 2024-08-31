6d105195f5a84a2a3646d49149d8cbfaa39845a1
Karlx28 <mo0436032023@unab.edu.sv>
2024-08-30 22:39:40 -0600

    Subida

---
+++ DataAccess/Connection/Connection_Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess.Connection
+{
+    //esto es la cadena de conexion
+    public class Connection_Database
+    {
+        private SqlConnection c = new SqlConnection("Data Source=DESKTOP-L4680KA\\SQLEXPRESS;Initial Catalog=CRUDD;Integrated Security=True;");
+        
+        public SqlConnection OpenConnection() 
+            // Esto abre la conexión
+        
+        {
+            if (c.State == ConnectionState.Closed) c.Open();
+            return c;
+        }
+
+        public SqlConnection CloseConnection()
+            // Esta cierra la conexión
+        {
+            if (c.State == ConnectionState.Open) c.Close();
+            return c;
+        }
+    }
+}
